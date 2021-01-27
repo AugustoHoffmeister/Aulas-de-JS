163412b788582fb0c031c89cd837cda8685f953d
AugustoHoffmeister <augustohof@yahoo.com>
2021-01-27 16:08:22 -0300

    Aula15
    Pratica15

---
+++ Aula15/Pratica15.js
@@ -0,0 +1,6 @@
+let num = [5,8,2,9,3]
+for(let i=0;i<num.length;i++ ){
+    console.log(`Indice: ${i} tem valor ${num[i]}`)
+}
+console.log(`Este array possue ${num.length} elementos`)
+console.log(`Valores em ordem crescente: ${num.sort()}`)
